42a5d81758044edce1e23f8b48f07a9926e942dd
tihidding <58736276+tihidding@users.noreply.github.com>
2021-10-05 13:49:42 +0200

    Vragen worden gedisplayed
    vragen worden nog niet uit XML gehaald maar er is een begin

--- Duzagotit/Scripts/Main.gd
+++ Duzagotit/Scripts/Main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 export (PackedScene) var QuestionBubble
+export (PackedScene) var Question
 var score
 
 func new_game():
@@ -11,9 +12,28 @@ func _ready():
 	randomize()
 	new_game()
 	
-func _on_QuestionSpawnTimer_timeout():
+func _on_QuestionSpawnTimer_timeout(): # spawns a question bubble for the player to click on
 	$Path2D/PathFollow2D.offset = randi()
 	var QB = QuestionBubble.instance()
+	QB.connect("_on_Click_Question", self, "_on_Click_Question")
 	add_child(QB)
-	#var direction = Vector2()
-	#QB.linear_velocity = Vector2(rand_range(QB.min_speed, QB.max_speed), 0)
+
+func _on_Click_Question(category):
+	print("gothretho")
+	get_New_Question(category)
+	print("hi", category)
+	$QuestionSpawnTimer.stop()
+	#summon question scene
+	var QuestionScene = Question.instance()
+	add_child(QuestionScene)
+	QuestionScene.get_node("B1").text = "hi"
+
+func get_New_Question(category):
+	var parser = XMLParser.new()
+	parser.open("res://test.xml")
+	parser.read();
+	parser.skip_section()
+	print(parser.get_current_line())
+	for x in range(parser.get_attribute_count()):
+		print(parser.get_attribute_name(x))
+	print(parser.get_node_name())
--- Duzagotit/Scripts/QuestionBubble.gd
+++ Duzagotit/Scripts/QuestionBubble.gd
@@ -1,12 +1,15 @@
 extends Area2D
+signal _on_Click_Question(question_category)
 
 export var min_speed = 30  # Minimum speed range.
 export var max_speed = 50  # Maximum speed range.
-
+var possible_categories
+var category
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	category = "waste"
 	pass
 
 
@@ -16,3 +19,9 @@ func _process(delta):
 	
 func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
+
+
+func _on_VraagBubbel_input_event(viewport, event, shape_idx):
+	if (event is InputEventMouseButton && event.pressed):
+		emit_signal("_on_Click_Question", category)
+		queue_free()
