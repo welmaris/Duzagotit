93841a01390af7c31fa643321ec0adf21479a0aa
tihidding <58736276+tihidding@users.noreply.github.com>
2021-10-06 14:22:55 +0200

    vragen kunnen beantwoord worden
    en worden uit JSON gehaald

--- Duzagotit/Scripts/Player.gd
+++ Duzagotit/Scripts/Player.gd
@@ -18,34 +18,34 @@ func start(pos):
 
 func _process(delta):
 	var velocity = Vector2()
+	if !get_parent().question_is_showing:
+		if Input.is_action_pressed("move_right"):
+		   velocity.x += 1
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1
 
-	if Input.is_action_pressed("move_right"):
-	   velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+			$AnimatedSprite.play()
+		else:
+			$AnimatedSprite.stop()
 
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
+		position += velocity * delta
+		position.x = clamp(position.x, 0, screen_size.x)
+		position.y = clamp(position.y, 0, screen_size.y)
 
-	position += velocity * delta
-	position.x = clamp(position.x, 0, screen_size.x)
-	position.y = clamp(position.y, 0, screen_size.y)
-
-	if velocity.x > 0:
-		$AnimatedSprite.animation = "walkR"
-	elif velocity.x < 0:
-		$AnimatedSprite.animation = "walkL"
-	elif velocity.y < 0:
-		$AnimatedSprite.animation = "walkU"
-	elif velocity.y > 0:
-		$AnimatedSprite.animation = "walkD"
+		if velocity.x > 0:
+			$AnimatedSprite.animation = "walkR"
+		elif velocity.x < 0:
+			$AnimatedSprite.animation = "walkL"
+		elif velocity.y < 0:
+			$AnimatedSprite.animation = "walkU"
+		elif velocity.y > 0:
+			$AnimatedSprite.animation = "walkD"
 
 func _on_Player_body_entered( _body ):
 	hide()
