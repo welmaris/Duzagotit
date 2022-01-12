9ad0184c5560e35f6afd3e8f426bdf0e6a8ac240
Esther ZR <estherzr97@gmail.com>
2022-01-12 21:10:44 +0100

    removed bad collisions in wall texturepack

--- Duzagotit/Scripts/HUD.gd
+++ Duzagotit/Scripts/HUD.gd
@@ -3,16 +3,23 @@ extends CanvasLayer
 var score = 0
 var goal = 150
 
+func start_game():
+	# Open house scene
+	pass
+
+# Reset the score to 0
 func reset_score():
 	score = 0
 	$ScoreLabel.text = "Score: " + str(score) + "/" + str(goal)
 
+# increase score
 func update_score(change: int):
 #	Animation for the change in score?
 	score += change
 	var new = str(score)
 	$ScoreLabel.text = "Score: " + str(score) + "/" + str(goal)
 
+# 
 func set_goal(newGoal : int):
 	goal = newGoal
 	$ScoreLabel.text = "Score: " + str(score) + "/" + str(goal)
